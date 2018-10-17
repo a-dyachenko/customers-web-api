cc41bfed5e5ec88407524cc014b947ee272446f5
Alexander Dyachenko <alexander.diatchenko@gmail.com>
2018-10-17 14:34:38 +0300

    added endpoints for geting customer statuses and saving comments for customer

--- src/main/java/customers_web_api/CustomersRESTController.java
+++ src/main/java/customers_web_api/CustomersRESTController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,7 @@ import customer_core.service.CustomerDataService;
 import customers_core.dao.CustomerCoreSessionProvider;
 import customers_core.db.CommentDB;
 import customers_core.db.CustomerDB;
+import customers_core.db.CustomerStatusDB;
 import customers_web_api.data.CustomerCommentJSON;
 
 @RestController
@@ -47,13 +50,37 @@ public class CustomersRESTController {
 				.getCustomerDataService(new CustomerCoreSessionProvider());
 		List<CommentDB> customerComments = customerDataService.getCustomerComments(customerId);
 
-		System.out.println("comments db size " + customerComments.size());
 		List<CustomerCommentJSON> commentsJSON = new ArrayList<CustomerCommentJSON>();
 
 		for (CommentDB comment : customerComments)
 			commentsJSON.add(new CustomerCommentJSON(comment.getId(), comment.getCommentText(), comment.getCreated()));
-		System.out.println("comments json size " + commentsJSON.size());
 		return commentsJSON;
 	}
 
+	@GetMapping(value = "/customers/statuses", produces = { "application/json" })
+	@ResponseBody
+	public List<CustomerStatusDB> getCustomerStatuses() {
+
+		CustomerDataService customerDataService = CustomerDataService
+				.getCustomerDataService(new CustomerCoreSessionProvider());
+		List<CustomerStatusDB> customerStatuses = customerDataService.getCustomerStatuses();
+
+		return customerStatuses;
+	}
+
+	// TODO: return new comment id if success or API error in case of error
+	@PostMapping(value = "/customers/{customerId}/comments")
+	public void saveCustomerComment(@RequestBody CommentDB newComment, @PathVariable int customerId) {
+		CustomerDataService customerDataService = CustomerDataService
+				.getCustomerDataService(new CustomerCoreSessionProvider());
+		
+		// check if customer exists
+		CustomerDB customer = customerDataService.getCustomerById(customerId);
+		
+		if (customer == null) 
+			return; //TODO: send invalid paramter
+		newComment.setCustomer(customer);
+		customerDataService.saveComment(newComment);
+
+	}
 }
--- src/test/java/customers_web_api/test/CustomersRESTControllerTest.java
+++ src/test/java/customers_web_api/test/CustomersRESTControllerTest.java
@@ -7,19 +7,27 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore
+/**
+ * "integration" type test, no actual data is checked with assertions TODO:
+ * check Mockito for actual unit testing
+ * 
+ * @author pwner
+ *
+ */
 public class CustomersRESTControllerTest {
 	private static final String DEV_SERVER_URL = "http://localhost:8080/customers-web-api";
-	private static final String CUSTOMERS_URI = "/customers/";
-	private static final String CUSTOMER_COMMENTS_URI = "/comments/";
+	private static final String CUSTOMERS_URI = "/customers";
+	private static final String CUSTOMER_COMMENTS_URI = "/comments";
+	private static final String CUSTOMER_STATUSES_URI = "/statuses";
 	protected Logger logger = LogManager.getLogger(getClass());
 
 	@Test
@@ -28,7 +36,7 @@ public class CustomersRESTControllerTest {
 		HttpClient client = HttpClientBuilder.create().build();
 
 		int customerId = 22;
-		HttpResponse response = client.execute(new HttpGet(DEV_SERVER_URL + CUSTOMERS_URI + customerId));
+		HttpResponse response = client.execute(new HttpGet(DEV_SERVER_URL + CUSTOMERS_URI + "/" + customerId));
 		int statusCode = response.getStatusLine().getStatusCode();
 		Assert.assertEquals((HttpStatus.SC_OK), statusCode);
 		String responseString = EntityUtils.toString(response.getEntity());
@@ -53,13 +61,66 @@ public class CustomersRESTControllerTest {
 		HttpClient client = HttpClientBuilder.create().build();
 		int customerId = 23;
 
-		String resourceURI = DEV_SERVER_URL + CUSTOMERS_URI + customerId + CUSTOMER_COMMENTS_URI;
+		String resourceURI = DEV_SERVER_URL + CUSTOMERS_URI + "/" + customerId + CUSTOMER_COMMENTS_URI;
 		logger.info("get customer comments uri " + resourceURI);
-		HttpResponse response = client
-				.execute(new HttpGet(resourceURI));
+		HttpResponse response = client.execute(new HttpGet(resourceURI));
 		int statusCode = response.getStatusLine().getStatusCode();
 		Assert.assertEquals((HttpStatus.SC_OK), statusCode);
 		String responseString = EntityUtils.toString(response.getEntity());
 		logger.info("response string " + responseString);
 	}
+
+	@Test
+	public void getCustomerStatusesTest() throws ClientProtocolException, IOException {
+
+		HttpClient client = HttpClientBuilder.create().build();
+
+		String resourceURI = DEV_SERVER_URL + CUSTOMERS_URI + CUSTOMER_STATUSES_URI;
+		logger.info("get customer statuses uri " + resourceURI);
+		HttpResponse response = client.execute(new HttpGet(resourceURI));
+		int statusCode = response.getStatusLine().getStatusCode();
+		Assert.assertEquals((HttpStatus.SC_OK), statusCode);
+		String responseString = EntityUtils.toString(response.getEntity());
+		logger.info("response string " + responseString);
+	}
+
+	@Test
+	public void getCustomerStatusesPostTest() throws ClientProtocolException, IOException {
+
+		HttpClient client = HttpClientBuilder.create().build();
+
+		String resourceURI = DEV_SERVER_URL + CUSTOMERS_URI + CUSTOMER_STATUSES_URI;
+		logger.info("get customer statuses uri " + resourceURI);
+		HttpResponse response = client.execute(new HttpPost(resourceURI));
+		int statusCode = response.getStatusLine().getStatusCode();
+		Assert.assertEquals((HttpStatus.SC_METHOD_NOT_ALLOWED), statusCode);
+		String responseString = EntityUtils.toString(response.getEntity());
+		logger.info("response string " + responseString);
+	}
+
+	@Test
+	public void saveCustomerComment() throws ClientProtocolException, IOException {
+
+		HttpClient client = HttpClientBuilder.create().build();
+		int customerId = 23;
+
+		String resourceURI = DEV_SERVER_URL + CUSTOMERS_URI + "/" + customerId + CUSTOMER_COMMENTS_URI;
+		logger.info("get customer comments uri " + resourceURI);
+		HttpPost httpPost = new HttpPost(resourceURI);
+
+		String commentJson = "{\"commentText\":\"test comment \"}";
+		StringEntity entity = new StringEntity(commentJson);
+	    httpPost.setHeader("Accept", "application/json");
+	    httpPost.setHeader("Content-type", "application/json");
+		
+		httpPost.setEntity(entity);
+		HttpResponse response = client.execute(httpPost);
+
+		int statusCode = response.getStatusLine().getStatusCode();
+		Assert.assertEquals((HttpStatus.SC_OK), statusCode);
+		String responseString = EntityUtils.toString(response.getEntity());
+		logger.info("response string " + responseString);
+
+	}
+
 }
